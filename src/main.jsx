1c9584d56715c952514d2fa0aaceaa85e19a3c55
Ednaira Viana <ednairaviana.evs@gmail.com>
2026-02-25 18:22:04 -0300

    import basic css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./css/reset.css";
+import "./css/root.css";
+import "./css/tokens.css";
+import "./css/components.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
